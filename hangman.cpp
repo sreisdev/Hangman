ed3e6729d263b6cbea36b77b7b9cb181b7a18622
Samuel Reis <samuel.reis@web.de>
2024-12-11 16:09:02 +0100

    review

--- hangman.cpp
+++ hangman.cpp
@@ -34,7 +34,6 @@ int main()
             }
             in=inputCharacter();
             if(!checkedGuessed(in, guessed)){
-                //Do rest
         
                if(!guessChar(in, tried, word)){
                    attempts++;
