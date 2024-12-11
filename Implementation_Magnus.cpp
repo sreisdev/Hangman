982c95e5107f9271343afc59b3e5c21df820be28
MagnusLundberg81 <karlmagnuslundberg@gmail.com>
2024-12-11 14:52:38 +0100

    Implemented printGame

---
+++ Implementation_Magnus.cpp
@@ -0,0 +1,79 @@
+#include "Header.h";
+#include <iostream>
+
+vector<vector<string>> hangman = {
+	{" +---+",
+	 " |    |",
+	 "      |",
+	 "      |",
+	 "      |",
+	 "      |",
+	"========"},
+{	" +---+"
+	" |   |"
+	" O   |"
+	"     |"
+	"     |"
+	"     |"
+	"========"},{
+	" +---+"
+	" |   |"
+	" O   |"
+	" |   |"
+	"     |"
+	"     |"
+	"========"},
+{	" +---+"
+	" |   |"
+	" O   |"
+	"/|   |"
+	"     |"
+	"     |"
+	"========"},
+{	" +---+"
+	" |   |"
+	" O   |"
+	"/|\  |"
+	"     |"
+	"     |"
+	"========"},
+{	" +---+"
+	" |   |"
+	" O   |"
+	"/|\  |"
+	"     |"
+	"     |"
+	"========"},
+{	" +---+"
+	" |   |"
+	" O   |"
+	"/|\  |"
+	"/    |"
+	"     |"
+	"========"},
+{	" +---+"
+	" |   |"
+	" O   |"
+	"/|\  |"
+	"/ \  |"
+	"     |"
+	"========"}
+};
+
+
+
+void printGame(vector<char> tried, int attempts, vector<char> guessed) {
+
+	system("csl");
+	cout << hangman[attempts][0] << endl;
+	cout << hangman[attempts][1] << endl;
+	cout << hangman[attempts][2] << endl;
+	cout << hangman[attempts][3] << endl;
+	cout << hangman[attempts][4] << endl;
+	cout << hangman[attempts][5] << endl;
+	cout << hangman[attempts][6] << endl;
+	for (char i : tried) cout << " " << i;
+	cout << endl;
+	cout << "Guessed: ";
+	for (char i : guessed) cout << " " << i;
+}
