6aae79ce9d4295a0b035d13f6bc69eaf84253784
Samuel Reis <samuel.reis@web.de>
2024-12-11 16:07:49 +0100

    end debugging

--- hangman.cpp
+++ hangman.cpp
@@ -5,52 +5,6 @@
 #include <cstdlib>
 #include "Header.h"
 
-//
-
-
-//using namespace std;
-////Gets a random word from the wordlist.txt file
-//vector<char> getRandomword(){
-//    return {'p', 'a', 'r', 't', 'y'};
-//}
-//
-//// Guess a character in words return true if guess was right and false if guess was wrong
-//bool guessChar(char c, vector<char>& tried, vector<char> word){
-// return true;
-//}
-// 
-////check if tried is the same as word if true the YOU WIN!!
-//bool checkCompleted(vector<char> tried, vector<char> word){
-//    return true;
-//}
-//// Print the state of the game, aka the hangman, the current tried vector and which chars been guessed
-//void printGame(vector<char> tried, int attempts, vector<char> guessed){
-//    //clear screen, print vector tried, print hangman, print guessed chars
-//    system("cls");
-//    cout << "TEST!";
-//}
-//// check if the char has been guessed already
-//bool checkedGuessed(char c, vector<char> guessed){
-//  return false;
-//}
-//// Print the request to put in new character and read character, return read character, check that it is not illegal input 
-//char inputCharacter(){
-//    return 'p';
-//}
-//// Start the game AGAIN! y/n
-//bool restartGame(char input){
-//    return true; //If yes, no return 0;
-//}
-//// Print question and get char for continue game y/n    
-//char getCharStartNewGame(){
-//    //print shit
-//    return 'y';
-//}
-//void resettried(vector<char>& tried){
-//    //make tried have same size as word and filled with '_'
-//}
-
-
 int main()
 {
     string filename = "wordlist.txt";
@@ -59,7 +13,7 @@ int main()
     vector<char> word;
     vector<char> tried;
     vector<char> guessed;
-    bool runGame=true, youwin;
+    bool runGame=true, youwin=false;
     bool alreadyGuessed = false;
     
     
@@ -68,7 +22,7 @@ int main()
      guessed.clear();
      resettried(tried, word.size());
      attempts=0;
-        while(attempts<maxAttempts){
+        while(attempts<=maxAttempts){
             if(checkCompleted(tried, word) ){
                 youwin=true;
                 break;
@@ -87,10 +41,6 @@ int main()
                }
                 guessed.push_back(in);
 
-               
-        
-        
-                    
             }else{
                 alreadyGuessed = true;
             }
